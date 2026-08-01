92b6d54b054cb8e31ce4c5f29141d6dae4400ac8
F1NiX <73650428+F1N1X@users.noreply.github.com>
2026-08-01 10:52:05 +0200

    feat: adding Serializable for Car/User-Objects

--- src/com/car_booking/car/CarDAO.java
+++ src/com/car_booking/car/CarDAO.java
@@ -1,8 +1,11 @@
 package car;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CarDao {
     Car[] getCars();
     Car findCarById(UUID id);
+
+    Optional<Car> getCarByRegistrationNumber(String number);
 }
