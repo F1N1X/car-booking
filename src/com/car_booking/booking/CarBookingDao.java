598f8b4a4dabac6dc1e12c0d732642434ebdc43d
F1NiX <73650428+F1N1X@users.noreply.github.com>
2026-08-13 21:41:06 +0200

    refactor: use List for saving CarBookings

--- src/com/car_booking/booking/CarBookingDao.java
+++ src/com/car_booking/booking/CarBookingDao.java
@@ -2,10 +2,11 @@ package booking;
 
 import car.Car;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface CarBookingDao {
-    CarBooking[] getBookings();
+    List<CarBooking> getBookings();
     CarBooking findBookingById(UUID bookingId);
     void saveBooking(CarBooking booking);
     void deleteBooking(UUID bookingId);
